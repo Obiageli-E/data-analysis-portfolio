47ed420fbf55c644184a457bbf483ea212169aab
Obiageli Iweanoge <obyiweanoge@gmail.com>
2026-06-05 13:25:13 +0100

    Delete sql/sql directory

--- sql/sql/Calories-burned.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  ROUND(session_duration::numeric, 1) AS session_hours,
-  ROUND(AVG(calories_burned), 2) AS avg_calories_burned
-FROM activelifestyle
-GROUP BY ROUND(session_duration::numeric, 1)
-ORDER BY session_hours ASC;
-
